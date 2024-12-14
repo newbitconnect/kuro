df3bffa2cfc12dcf5527ef45633c69a5195b6c85
newbitconnect <newbitconnect@proton.me>
2024-12-14 21:50:39 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Temporizador
+const countdown = () => {
+  const launchDate = new Date("2024-12-25T00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = launchDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(gap / day);
+  const hours = Math.floor((gap % day) / hour);
+  const minutes = Math.floor((gap % hour) / minute);
+  const seconds = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+};
+
+setInterval(countdown, 1000);
